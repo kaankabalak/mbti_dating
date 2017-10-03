4df86501c0792b20631735be749f949cf396769c
Kaan <kaankabalak@gmail.com>
2017-10-02 23:04:20 -0700

    updated user properties

--- server/models/user.js
+++ server/models/user.js
@@ -2,8 +2,10 @@
 var mongoose = require('mongoose');
 // create the schema
 var UserSchema = new mongoose.Schema({
- username: String,
- score: Number,
- image: String
+ name: {type: String, required: true, maxlength: 50},
+ age: {type: Number},
+ image: {type: String},
+ description: {type: String, maxlength: 150},
+ personality: {type: String}
 })
 var User = mongoose.model('User', UserSchema); // We are setting this Schema in our Models as 'User'
